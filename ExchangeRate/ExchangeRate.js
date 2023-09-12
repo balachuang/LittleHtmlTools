d08546c644815e5a83724d7b1586804708d9633d
balachuang <balachuang@gmail.com>
2023-09-12 21:37:58 +0800

    set currency input to cookie

--- ExchangeRate/ExchangeRate.js
+++ ExchangeRate/ExchangeRate.js
@@ -1,17 +1,21 @@
-let currRates = [];
+// let currRates = [];
+let currTemplate = '{excIn} {currSrc}可以換 {excRes} {currTrg}';
 
 $(document).ready(function(){
 	parseCurrencyRate();
 
 	$('#addCurrency').click(inputCurrency);
 	$('#showCurrency').click(showCurrency);
+	$('#clearCurrency').click(clearCurrency);
+	$('#input-curr').change(parseCurrencyRate);
 });
 
 function inputCurrency()
 {
 	let curr = prompt('請輸入貨幣名稱', '美金');
-	let rate = prompt('請輸入一 ' + curr + ' 等於多少台幣?', '1.0');
+	let rate = prompt('請輸入一 ' + curr + ' 等於多少台幣?', '30.0');
 	addCurrencyToCookie(curr, rate);
+	parseCurrencyRate();
 }
 
 function addCurrencyToCookie(curr, rate)
@@ -28,7 +32,9 @@ function addCurrencyToCookie(curr, rate)
 
 function parseCurrencyRate()
 {
-	currRates = [];
+	let excInput = eval($('#input-curr').val());
+	$('#output-curr-area').html('');
+	// currRates = [];
 	let currRateStr = Cookies.get('currency-rate');
 	if (currRateStr != undefined)
 	{
@@ -36,15 +42,55 @@ function parseCurrencyRate()
 		for (let n=0; n<currRateStrAry.length; ++n)
 		{
 			let currRateStrAryItem = currRateStrAry[n].split(':');
-			currRates.push({
-				currName: currRateStrAryItem[0],
-				currRate: currRateStrAryItem[1]
-			});
+			let currName = currRateStrAryItem[0];
+			let currRate = eval(currRateStrAryItem[1]);
+			// currRates.push({
+			// 	currName: currName,
+			// 	currRate: currRate
+			// });
+
+			$('#output-curr-area').append(generateExcHtml('新台幣', currName, excInput, excInput*1.0/currRate));
+			$('#output-curr-area').append(generateExcHtml(currName, '新台幣', excInput, excInput*currRate));
 		}
 	}
 }
 
+function generateExcHtml(currSrc, currTrg, excIn, excRes)
+{
+	return '<div class="curr-exc">' + generateExcStr(currSrc, currTrg, excIn, excRes) + '</div>';
+}
+
+function generateExcStr(currSrc, currTrg, excIn, excRes)
+{
+	return currTemplate
+			.replace('{currSrc}', currSrc)
+			.replace('{currTrg}', currTrg)
+			.replace('{excIn}', Math.round(10000.0 * excIn) / 10000)
+			.replace('{excRes}', Math.round(10000.0 * excRes) / 10000);
+}
+
 function showCurrency()
 {
-	alert(Cookies.get('currency-rate').replace(/,/g, '\r\n'));
+	let dispStr = '';
+	let currRateStr = Cookies.get('currency-rate');
+	if (currRateStr != undefined)
+	{
+		let currRateStrAry = currRateStr.split(',');
+		for (let n=0; n<currRateStrAry.length; ++n)
+		{
+			let currRateStrAryItem = currRateStrAry[n].split(':');
+			let currName = currRateStrAryItem[0];
+			let currRate = eval(currRateStrAryItem[1]);
+			dispStr += generateExcStr(currName, '新台幣', 1, currRate) + '\r\n';
+		}
+	}
+
+	// alert(Cookies.get('currency-rate').replace(/,/g, '\r\n'));
+	alert(dispStr);
+}
+
+function clearCurrency()
+{
+	Cookies.remove('currency-rate');
+	parseCurrencyRate();
 }
