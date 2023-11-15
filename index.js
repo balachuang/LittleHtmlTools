3a6469a64d58aa19a08dc63a7478abce3fe5f0d0
balachuang <balachuang@gmail.com>
2023-11-15 22:25:56 +0800

    add Online Bookmark

--- index.js
+++ index.js
@@ -1,20 +1,14 @@
 let tools = [
-	{
-		name: 'Open Multiple Urls',
-		id: 'MultipleLinks'
-	},
-	{
-		name: 'Exchange Rate',
-		id: 'ExchangeRate'
-	},
+	{ name: 'Open Multiple Urls',   id: 'MultipleLinks'  },
+	{ name: 'Exchange Rate',        id: 'ExchangeRate'   },
+	{ name: 'My Online Bookmarks',  id: 'OnlineBookmark' },
 ];
+
 let htmlTemplate = '<li><a href="{id}/{id}.html">{name}</a></li>';
 
 $(document).ready(function(){
 	for (var n=0; n<tools.length; ++n) {
-		let toolHtml = htmlTemplate
-							.replace(/\{id\}/g, tools[n].id)
-							.replace(/\{name\}/g, tools[n].name);
+		let toolHtml = htmlTemplate.replace(/\{id\}/g, tools[n].id).replace(/\{name\}/g, tools[n].name);
 		$('#tools-links').append(toolHtml);
 	}
 });
