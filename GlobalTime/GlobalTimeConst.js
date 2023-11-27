7847c70f7e66c466ae5cf6418818d4f01750484c
balachuang <balachuang@gmail.com>
2023-11-27 19:58:45 +0800

    make map full screen

--- GlobalTime/GlobalTimeConst.js
+++ GlobalTime/GlobalTimeConst.js
@@ -16,14 +16,14 @@ const posDomTemp4 = '<div class="posArrow"></div>';
 const positions = [
 	{
 		name: '台北 / Taipei',
-		x: 41.4, y: 51.6, timeZone: 8, offset: {x: 1, y: -0.5},
+		x: 41.3, y: 50.9, timeZone: 8, offset: {x: 1, y: -0.5},
 		references: [
 			'https://www.google.com.tw/maps/@24.9795201,121.5405383,92a,35y,194.77h,62.54t/data=!3m1!1e3?entry=ttu',
 			'https://www.google.com.tw/maps/@25.0261969,121.5598102,647a,35y,32.34h,67.36t/data=!3m1!1e3?entry=ttu'
 		]
 	},{
 		name: '東京 / Tokyo',
-		x: 46.6, y: 45.6, timeZone: 9, offset: {x: 4, y: 0},
+		x: 46.2, y: 44.9, timeZone: 9, offset: {x: 4, y: 0},
 		references: [
 			'https://www.google.com.tw/maps/@35.7108445,139.7961992,3a,90y,37.26h,104.46t/data=!3m6!1e1!3m4!1s3_OyvdBSsyuZPrwox4fRFw!2e0!7i16384!8i8192?entry=ttu',
 			'https://www.google.com.tw/maps/@35.6593256,139.7451034,3a,75y,152.32h,131.64t/data=!3m6!1e1!3m4!1sAVQYJqPn9Ok5yfPCLHJRJA!2e0!7i16384!8i8192?entry=ttu',
@@ -32,7 +32,7 @@ const positions = [
 		]
 	},{
 		name: '北海道 / Hokkaido',
-		x: 47.6, y: 40.6, timeZone: 9, offset: {x: 2, y: -2},
+		x: 47.2, y: 39.9, timeZone: 9, offset: {x: 2, y: -2},
 		references: [
 			'https://www.google.com.tw/maps/@41.7721508,140.7069229,3a,39.1y,142.41h,97.9t/data=!3m6!1e1!3m4!1s-Gdif6W-eviPEhMFf9SK3A!2e0!7i16384!8i8192?entry=ttu',
 			'https://www.google.com.tw/maps/@43.7685518,142.4774965,3a,47y,100.57h,97.24t/data=!3m6!1e1!3m4!1s2nyheTy2Qxp4CXcY4MEU0A!2e0!7i13312!8i6656?entry=ttu',
@@ -42,7 +42,7 @@ const positions = [
 		]
 	},{
 		name: '京都, 大阪, 神戶',
-		x: 46.0, y: 46.2, timeZone: 9, offset: {x: -8, y: -6},
+		x: 45.6, y: 45.5, timeZone: 9, offset: {x: -8, y: -6},
 		references: [
 			'https://www.google.com.tw/maps/@34.6692635,135.5013025,3a,75y,174.61h,111.43t/data=!3m6!1e1!3m4!1s7ksTbl8b1t4kWBQlQV7tGQ!2e0!7i13312!8i6656?entry=ttu',
 			'https://www.google.com.tw/maps/@34.9940965,135.7854214,2a,90y,306.7h,91.7t/data=!3m6!1e1!3m4!1shHkLHDIby-kpTuEZhE81Fg!2e0!7i13312!8i6656?entry=ttu',
@@ -54,7 +54,7 @@ const positions = [
 		]
 	},{
 		name: '北海道 / Hokkaido',
-		x: 47.6, y: 40.6, timeZone: 9, offset: {x: 2, y: -2},
+		x: 47.2, y: 39.9, timeZone: 9, offset: {x: 2, y: -2},
 		references: [
 			'https://www.google.com.tw/maps/@41.7721508,140.7069229,3a,39.1y,142.41h,97.9t/data=!3m6!1e1!3m4!1s-Gdif6W-eviPEhMFf9SK3A!2e0!7i16384!8i8192?entry=ttu',
 			'https://www.google.com.tw/maps/@43.7685518,142.4774965,3a,47y,100.57h,97.24t/data=!3m6!1e1!3m4!1s2nyheTy2Qxp4CXcY4MEU0A!2e0!7i13312!8i6656?entry=ttu',
@@ -64,7 +64,7 @@ const positions = [
 		]
 	},{
 		name: '北京',
-		x: 40.1, y: 43.5, timeZone: 8, offset: {x: -8, y: 0.5},
+		x: 39.7, y: 42.8, timeZone: 8, offset: {x: -8, y: 0.5},
 		references: [
 			'https://www.google.com.tw/maps/@39.914342,116.3971927,2a,75y,359.57h,102.28t/data=!3m7!1e1!3m5!1smvIxTGcVEYQAAAREq-bR4A!2e0!6shttps:%2F%2Fstreetviewpixels-pa.googleapis.com%2Fv1%2Fthumbnail%3Fpanoid%3DmvIxTGcVEYQAAAREq-bR4A%26cb_client%3Dmaps_sv.tactile.gps%26w%3D203%26h%3D100%26yaw%3D200.95447%26pitch%3D0%26thumbfov%3D100!7i13312!8i6656?entry=ttu',
 			'https://www.google.com.tw/maps/@40.3608848,116.0158768,3a,75y,211.66h,84.18t/data=!3m8!1e1!3m6!1sAF1QipPNM0zRvEIC2cTb9N-izMGQlvAXzXbHdhZVWlFC!2e10!3e11!6shttps:%2F%2Flh5.googleusercontent.com%2Fp%2FAF1QipPNM0zRvEIC2cTb9N-izMGQlvAXzXbHdhZVWlFC%3Dw203-h100-k-no-pi0-ya8.705296-ro-0-fo100!7i8704!8i4352?entry=ttu',
@@ -74,7 +74,7 @@ const positions = [
 		]
 	},{
 		name: '香港 / Hong Kong',
-		x: 40.2, y: 52.7, timeZone: 8, offset: {x: -7.5, y: 1},
+		x: 39.8, y: 52.3, timeZone: 8, offset: {x: -7.5, y: 1},
 		references: [
 			'https://www.google.com.tw/maps/@22.2710413,114.1503332,3a,73.7y,355.46h,79.73t/data=!3m8!1e1!3m6!1sAF1QipMiGztBEfFYWEmY4mHVGvFFoBEMLv5G7e2a6865!2e10!3e11!6shttps:%2F%2Flh5.googleusercontent.com%2Fp%2FAF1QipMiGztBEfFYWEmY4mHVGvFFoBEMLv5G7e2a6865%3Dw203-h100-k-no-pi-0-ya145.51648-ro0-fo100!7i10240!8i5120?entry=ttu',
 			'https://www.google.com.tw/maps/@22.2929688,114.1732326,3a,75y,199.5h,103.82t/data=!3m7!1e1!3m5!1sOh5zsDc0COGYnXWRxP_jQA!2e0!6shttps:%2F%2Fstreetviewpixels-pa.googleapis.com%2Fv1%2Fthumbnail%3Fpanoid%3DOh5zsDc0COGYnXWRxP_jQA%26cb_client%3Dmaps_sv.tactile.gps%26w%3D203%26h%3D100%26yaw%3D108.070244%26pitch%3D0%26thumbfov%3D100!7i16384!8i8192?entry=ttu',
@@ -82,7 +82,7 @@ const positions = [
 		]
 	},{
 		name: '瑞士 / Switzerland',
-		x: 10.3, y: 38.5, timeZone: 1, offset: {x: 0.5, y: 0.5},
+		x: 9.9, y: 37.8, timeZone: 1, offset: {x: 0.5, y: 0.5},
 		references: [
 			'https://www.google.com.tw/maps/@46.5179569,6.629601,98a,35y,16.7h,40.66t/data=!3m1!1e3?entry=ttu',
 			'https://www.google.com.tw/maps/@46.5183788,6.6301591,3a,75y,351.61h,119.31t/data=!3m6!1e1!3m4!1skrzhYIBBemitJNHIirhzWQ!2e0!7i13312!8i6656?entry=ttu',
@@ -96,7 +96,7 @@ const positions = [
 		]
 	},{
 		name: '費爾班克斯 / Fairbanks',
-		x: 66.7, y: 21.5, timeZone: -9, offset: {x: 1, y: 0.5},
+		x: 66.3, y: 20.8, timeZone: -9, offset: {x: 1, y: 0.5},
 		references: [
 			'https://www.google.com.tw/maps/@64.8605397,-147.6991619,55a,35y,223.07h,70.44t/data=!3m1!1e3?entry=ttu',
 			'https://www.google.com.tw/maps/@64.8597333,-147.7015103,3a,75y,308.7h,89.59t/data=!3m8!1e1!3m6!1sAF1QipOJunSNYR7QUD2KmgXk7folVnVvUuCR_AgDov1e!2e10!3e11!6shttps:%2F%2Flh5.googleusercontent.com%2Fp%2FAF1QipOJunSNYR7QUD2KmgXk7folVnVvUuCR_AgDov1e%3Dw203-h100-k-no-pi0-ya259.66318-ro-0-fo100!7i8704!8i4352?entry=ttu',
@@ -104,7 +104,7 @@ const positions = [
 		]
 	},{
 		name: '拉斯維加斯 / Las Vegas',
-		x: 76.1, y: 45.4, timeZone: -8, offset: {x: 0.5, y: 0.5},
+		x: 75.7, y: 44.7, timeZone: -8, offset: {x: 0.5, y: 0.5},
 		references: [
 			'https://www.google.com.tw/maps/@36.1122134,-115.1769481,270a,35y,26.19h,73.32t/data=!3m1!1e3?entry=ttu',
 			'https://www.google.com.tw/maps/@36.1215393,-115.1690917,3a,75y,105.23h,109.25t/data=!3m8!1e1!3m6!1sAF1QipMkg3Zffq6OUyIEuM_aOouTRIJutMnaySrbiUlY!2e10!3e11!6shttps:%2F%2Flh5.googleusercontent.com%2Fp%2FAF1QipMkg3Zffq6OUyIEuM_aOouTRIJutMnaySrbiUlY%3Dw203-h100-k-no-pi-0-ya17.334097-ro-0-fo100!7i8192!8i4096?entry=ttu',
@@ -113,7 +113,7 @@ const positions = [
 		]
 	},{
 		name: '舊金山 / San Francisco',
-		x: 74.1, y: 44.7, timeZone: -8, offset: {x: -10, y: 0.5},
+		x: 73.7, y: 44.0, timeZone: -8, offset: {x: -10, y: 0.5},
 		references: [
 			'https://www.google.com.tw/maps/@37.8076006,-122.4110992,76a,35y,31.64h,66.53t/data=!3m1!1e3?entry=ttu',
 			'https://www.google.com.tw/maps/@37.8086682,-122.4101669,3a,75y,350.51h,89.61t/data=!3m8!1e1!3m6!1sAF1QipMnlXKOg8ptYiB-rMGJq1FgfDYnJz-7ZxpzSdAx!2e10!3e11!6shttps:%2F%2Flh5.googleusercontent.com%2Fp%2FAF1QipMnlXKOg8ptYiB-rMGJq1FgfDYnJz-7ZxpzSdAx%3Dw203-h100-k-no-pi-0-ya19.953331-ro-0-fo100!7i7200!8i3600?entry=ttu',
@@ -124,7 +124,7 @@ const positions = [
 		]
 	},{
 		name: '西雅圖 / Seattle',
-		x: 74.1, y: 39.2, timeZone: -8, offset: {x: -9, y: -1},
+		x: 73.7, y: 38.5, timeZone: -8, offset: {x: -9, y: -1},
 		references: [
 			'https://www.google.com.tw/maps/@47.6208581,-122.3484483,3a,75y,235.58h,134.92t/data=!3m7!1e1!3m5!1stBLGYnKb8CgZB9T2bo0ziQ!2e0!6shttps:%2F%2Fstreetviewpixels-pa.googleapis.com%2Fv1%2Fthumbnail%3Fpanoid%3DtBLGYnKb8CgZB9T2bo0ziQ%26cb_client%3Dmaps_sv.tactile.gps%26w%3D203%26h%3D100%26yaw%3D98.84805%26pitch%3D0%26thumbfov%3D100!7i16384!8i8192?entry=ttu',
 			'https://www.google.com.tw/maps/@47.608788,-122.340647,2a,75y,197.73h,107.32t/data=!3m6!1e1!3m4!1sL1OnTzm4oo0Zj1qAb4QNhA!2e0!7i13312!8i6656?entry=ttu',
@@ -132,7 +132,7 @@ const positions = [
 		]
 	},{
 		name: '洛杉磯 / Los Angeles',
-		x: 74.8, y: 46.3, timeZone: -8, offset: {x: -5.5, y: 4},
+		x: 74.4, y: 45.6, timeZone: -8, offset: {x: -5.5, y: 4},
 		references: [
 			'https://www.google.com.tw/maps/@34.1016013,-118.3409685,3a,75y,357.75h,105.65t/data=!3m6!1e1!3m4!1sbs6opyW_oCSErAxrSBc1sQ!2e0!7i16384!8i8192?entry=ttu',
 			'https://www.google.com.tw/maps/@34.1289299,-118.3224647,326a,35y,7.03h,71.88t/data=!3m1!1e3?entry=ttu'
